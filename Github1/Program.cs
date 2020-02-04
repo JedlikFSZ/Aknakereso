1f17f2240bb89e84a39edc773ae1e6a530ed7b79
Fehér.Szabolcs1 <Fehr.Szabolcs1@C102-13>
2020-02-04 13:50:59 +0100

    Kirajzoló,Feltöltés

--- Github1/Program.cs
+++ Github1/Program.cs
@@ -8,16 +8,38 @@ namespace Github1
 {
     class Program
     {
-        char[,] Tábla = new char[10,10];
+        
         static void Main(string[] args)
         {
-            Feltöltés();   
+            char[,] Tábla = new char[10,10];
+            Feltöltés(Tábla);   
+            Kirajzoló(Tábla);
 
             Console.ReadKey();
         }
-        static void Feltöltés()
+        static void Feltöltés(char[,]Tábla)
+        {
+            for (int sor = 0; sor < pálya.GetLength(0); sor++)
             {
 
+                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                {
+                    pálya[sor, oszlop] = '_';
+                }
             }
+        }
+        static void Kirajzoló(char[,]Tábla)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int sor = 0; sor < pálya.GetLength(0); sor++)
+            {
+                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                {
+                    Console.Write($"{pálya[sor, oszlop]}|");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
