4ffc8574cc4a4796276fa8a4e10bb2954ab32e4b
Fehér Szabolcs <szabolcsfeher22@gmail.com>
2020-02-13 16:51:52 +0100

    Keddi órai munka

--- Github1/Program.cs
+++ Github1/Program.cs
@@ -4,55 +4,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Github1
+namespace Aknakereso_javitott
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            char[,] Tábla = new char[10,10];
-            Feltöltés(Tábla);   
-            Kirajzoló(Tábla);
-
+            char[,] pálya = new char[12, 12];
+            Feltöltés(pálya);
+            Bombasorsoló(pálya);
+            Kirajzoló(pálya, true);
+            Ellenőrző(pálya);
+            int lépx;
+            int lépy;
+            do
+            {
+                Lépés(pálya, out lépx, out lépy);
+            } while (pálya[lépx, lépy] != 'B');
             Console.ReadKey();
         }
-        static void Feltöltés(char[,]Tábla)
-        {
-            for (int sor = 0; sor < Tábla.GetLength(0); sor++)
-            {
 
-                for (int oszlop = 0; oszlop < Tábla.GetLength(1); oszlop++)
+        static void Feltöltés(char[,] pálya)
+        {
+
+            for (int i = 0; i < pálya.GetLength(0); i++)
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
                 {
-                    Tábla[sor, oszlop] = '_';
+                    pálya[i, j] = '_';
                 }
             }
-            Random gép = new Random();
-            int x2;
-            int x;
-             for (int sor = 0; sor < 10; sor++)
-             {
-                do
-                {
-                    x = gép.Next(1, 10);
-                    x2 = gép.Next(1, 10);
-                } while (Tábla[x, x2] == 'B');
-                Tábla[x, x2] = 'B';
-             }
+        }
+
+        static void Lépés(char[,] pálya, out int lépx, out int lépy)
+        {
+            Console.WriteLine("Kérem a sorszámot.");
+            lépx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kérem az oszlopszámot.");
+            lépy = int.Parse(Console.ReadLine());
+            if (pálya[lépx, lépy] == 'B')
+            {
+                Kirajzoló(pálya, true);
+                Console.WriteLine("Felrobbantál.");
+            }
+            else if (pálya[lépx,lépy]!='_' || pálya[lépx,lépy]!='B' || pálya[lépx,lépy]!='X')
+            {
+                Console.WriteLine("Gratulálok, nyertél!");
+            }
+            else
+            {
+                pálya[lépx, lépy] = char.Parse(BombaSzomszédSzám(pálya, lépx, lépy).ToString());
+                Kirajzoló(pálya, false);
+            }
 
         }
-        static void Kirajzoló(char[,]Tábla)
+
+        static void Bombasorsoló(char[,] pálya)
         {
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.ForegroundColor = ConsoleColor.White;
-            for (int sor = 0; sor <Tábla.GetLength(0); sor++)
+            Random gép = new Random();
+            Console.WriteLine("Add meg a bombaszámot.");
+            int bombaszám = int.Parse(Console.ReadLine());
+            int sor;
+            int oszlop;
+            for (int i = 0; i < bombaszám; i++)
             {
-                for (int oszlop = 0; oszlop < Tábla.GetLength(1); oszlop++)
+                do
                 {
-                    Console.Write($"{Tábla[sor, oszlop]}|");
+                    sor = gép.Next(1,11);
+                    oszlop = gép.Next(1,11);
+                } while (pálya[sor, oszlop] == 'B');
+                pálya[sor, oszlop] = 'B';
+            }
+        }
+
+        static void Kirajzoló(char[,] pálya, bool legyenbomba)
+        {
+            for (int i = 1; i < pálya.GetLength(0)-1; i++)
+            {
+                for (int j = 1; j < pálya.GetLength(1)-1; j++)
+                {
+                    if (!legyenbomba)
+                    {
+                        if (pálya[i, j] == 'B')
+                        {
+                            Console.Write('_');
+                        }
+                        else
+                        {
+                            Console.Write(pálya[i, j]);
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(pálya[i, j]);
+                    }
+
+                    Console.Write('|');
                 }
                 Console.WriteLine();
             }
+        }
+
+        static int BombaSzomszédSzám(char[,] pálya, int lépx, int lépy)
+        {
+            int bombadb = 0;
+            for (int i = lépx - 1; i <= lépx + 1; i++)
+            {
+                for (int j = lépy - 1; j <= lépy + 1; j++)
+                {
+                    if (pálya[i,j]=='B')
+                    {
+                        bombadb++;
+                    }
+                }
+            }
+            return bombadb;
+        }
+
+        static int Ellenőrző(char[,] pálya)
+        {
+            int számoló = 0;
+            for (int i = 0; i < pálya.GetLength(0); i++)
+            {
+                for (int j = 0; j < pálya.GetLength(1); j++)
+                {
+                    if (pálya[i,j]=='_')
+                    {
+                        számoló++;
+                    }
+                }
+            }
+            return számoló;
         }
     }
 }
